ecee0cec1e8843d2f5417b07446e19033f6354ba
Zoey Frakes <zfrakes811@gmail.com>
2020-01-28 19:15:12 -0500

    updated readme, fixed index.js

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Developer-Profile-Generator
+The Developer Profile Generator is an application that generates a PDF profile from a GitHub username. The application invokes the following command: node index.js. First, the user enters their GitHub Username. Second, the user needs to select their favorite color from the following options green, red, blue, and pink. Once the color is selected, the chosen color will be the background color for cards, and the PDF will  populate with the following:
+
+*Profile image
+*User name
+*Links to the following: 
+    *User location via Google Maps
+    *User GitHub profile
+    *User blog
+    *User bio
+*Number of public repositories
+*Number of followers
+*Number of users following
+
+This application is distributed in the hope that it will be a creative way to generate a person’s GitHub profile. 
+
+## Technologies/Languages used
+* HTML
+* CSS
+* Node index.js
+
+## Launch
+To launch this application click or copy and paste the following link in your browser. https://zfrakes.github.io/Developer-Profile-Generator/.
+
+## Author
+This application was created by Zoey Frakes. To contact please email at zfrakes811@gmail.com
+
+## License
+Developer Proflie Generator  is free software; you can redistribute it and/or modify it. Copyright © 2020 Zoey Frakes. All Rights Reserved
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-var pdf = require('html-pdf');
-var generateHTML = require("./generateHTML.js");
-var github = require('octonode');
-var inquirer=require("inquirer")
-var prompt = inquirer.createPromptModule();
+const pdf = require('html-pdf');
+const generateHTML = require("./generateHTML.js");
+const github = require('octonode');
+const inquirer=require("inquirer")
+const prompt = inquirer.createPromptModule();
 
 const questions = [
 { 
@@ -27,19 +27,19 @@ function writeToFile(fileName, data) {
 function init() {
 prompt(questions).then((data)=>{
     // console.log(data)
-    var client = github.client();
+    const client = github.client();
     client.get('/users/'+ data["github"], {}, function (err, status, body, headers) {
         // console.log(body); 
-       var follower=body["followers"]
-       var following=body["following"]
-       var repos=body["public_repos"]
-       var bio =body["bio"]
-       var location =body["location"]
-       var name =body["name"]
-       var company = body["company"]
-       var blog =body["blog"]
-       var githubprofile =body["url"]
-       var image =body["avatar_url"]
+       const follower=body["followers"]
+       const following=body["following"]
+       const repos=body["public_repos"]
+       const bio =body["bio"]
+       const location =body["location"]
+       const name =body["name"]
+       const company = body["company"]
+       const blog =body["blog"]
+       const githubprofile =body["url"]
+       const image =body["avatar_url"]
        data.followers=follower
        data.following=following
        data.repos=repos
@@ -59,8 +59,8 @@ fs.writeFile('page.html', html_text, (err) => {
 
     // success case, the file was saved
     // console.log('Html saved!');
-var html = fs.readFileSync('./page.html', 'utf8');
-var options = { format: 'Letter' };
+const html = fs.readFileSync('./page.html', 'utf8');
+const options = { format: 'Letter' };
 pdf.create(html, options).toFile('./page.pdf', function(err, res) {
   if (err) return console.log(err);
 //   console.log(res); // { filename: '/app/businesscard.pdf' }
